360f2875c820bee95829ff725e704d8e97555ebe
Peter Selten <peter.selten98@gmail.com>
2015-10-04 12:47:00 +0200

    Classes toegevoegd

---
+++ src/main/java/Verbindingen.java
@@ -0,0 +1,17 @@
+public class Verbindingen {
+
+	private int VerbindingID;
+
+	private Locatie Startpunt;
+
+	private Locatie Eindpunt;
+
+	public boolean getToegang() {
+		return false;
+	}
+
+	public double getVertraging() {
+		return 0;
+	}
+
+}
