d39194c38a8e41cfcd358df403af96157ee35148
Peter Selten <peter.selten98@gmail.com>
2015-10-05 12:42:51 +0200

    Software design opnieuw verandert, methodes uitgewerkt.

--- src/main/java/Verbinding.java
+++ src/main/java/Verbinding.java
@@ -1,10 +1,10 @@
 public class Verbinding {
-
 	private Plaats Startpunt;
-
 	private Plaats Eindpunt;
-
-	private float Afstand;
+	private int Afstand;
+    private double Vertragingstijd;
+    private String Vertragingssoort;
+    private boolean Toegang;
 
 	public boolean getToegang() {
 		return false;
@@ -14,8 +14,57 @@ public class Verbinding {
 		return 0;
 	}
 
-	public int creerVerbinding() {
-		return 0;
-	}
+	public void creerVerbinding(Plaats start, Plaats eind, int dist) {
+        Startpunt = start;
+        Eindpunt = eind;
+        Afstand = dist;
+    }
 
+    public Plaats getStartpunt() {
+        return Startpunt;
+    }
+
+    public Plaats getEindpunt() {
+        return Eindpunt;
+    }
+
+    public int getAfstand() {
+        return Afstand;
+    }
+
+    public void setVertragingsTijd(double time) {
+        Vertragingstijd = time;
+    }
+
+    public double getVertragingsTijd() {
+        return Vertragingstijd;
+    }
+
+    public void setVertragingsSoort(String soort) {
+        Vertragingssoort = soort;
+    }
+
+    public String getVertragingsSoort() {
+        return Vertragingssoort;
+    }
+
+    public boolean openVerbinding() {
+        if(Toegang == true) return false;
+        else {
+            Toegang = true;
+            return true;
+        }
+    }
+
+    public boolean sluitVerbinding() {
+        if(Toegang == false) return false;
+        else {
+            Toegang = false;
+            return true;
+        }
+    }
+
+    public boolean getToegankelijk() {
+        return Toegang;
+    }
 }
--- src/main/java/Verbindingen.java
+++ src/main/java/Verbindingen.java
@@ -1,20 +1,55 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Verbindingen {
-	private int VerbindingID;
-	private Locatie Startpunt;
-	private Locatie Eindpunt;
+    private HashMap distances = new HashMap();
+    private ArrayList verbindingen = new ArrayList();
 
 	public Verbindingen() {
-        ArrayList<Plaats> plaatsen = Locatie.Plaatsen;
+        distanceArrayCreator();
+        verbindingCreator();
     }
 
-	public boolean getToegang() {
-		return false;
-	}
+    private void distanceArrayCreator() {
+        HashMap<String,Integer> UtrechtFrom = new HashMap();
+        UtrechtFrom.put("Arnhem",50000);
+        UtrechtFrom.put("Nijmegen",75000);
+        UtrechtFrom.put("Elst",65000);
+        HashMap<String,Integer> ArnhemFrom = new HashMap();
+        ArnhemFrom.put("Utrecht",60000);
+        ArnhemFrom.put("Nijmegen",20000);
+        ArnhemFrom.put("Elst", 10000);
+        HashMap<String,Integer> NijmegenFrom = new HashMap();
+        NijmegenFrom.put("Utrecht",70000);
+        NijmegenFrom.put("Arnhem",25000);
+        NijmegenFrom.put("Elst", 13000);
+        HashMap<String,Integer> ElstFrom = new HashMap();
+        ElstFrom.put("Utrecht",70000);
+        ElstFrom.put("Arnhem",12000);
+        ElstFrom.put("Nijmegen", 12000);
 
-	public double getVertraging() {
-		return 0;
-	}
+        distances.put("Utrecht",UtrechtFrom);
+        distances.put("Arnhem",ArnhemFrom);
+        distances.put("Nijmegen",NijmegenFrom);
+        distances.put("Elst",ElstFrom);
+    }
 
+    private void verbindingCreator() {
+        int i = 0;
+        ArrayList<Plaats> plaatsen = Locatie.Plaatsen;
+        for(Plaats plaats : plaatsen) {
+            HashMap data = (HashMap) distances.get(plaats);
+            for(Object p2 : data.keySet()) {
+                int dist = (Integer) data.get(p2);
+                Plaats p2name = null;
+                for(Plaats plaats2 : plaatsen)
+                    if (plaats2.getPlaatsNaam().equals(p2.toString())) p2name = plaats2;
+                Verbinding verbinding = new Verbinding();
+                verbinding.creerVerbinding(plaats,p2name,dist);
+
+                verbindingen.add(i,verbinding);
+                i++;
+            }
+        }
+    }
 }
